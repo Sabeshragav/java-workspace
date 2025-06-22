5db88e9ce91b1ec9abd415c9d9b8e1aae22437dc
Sabeshragav <sabeshragav289@gmail.com>
2025-06-22 18:36:09 +0530

    Add palindrome checker and update regex validation in check.java

--- check.java
+++ check.java
@@ -3,6 +3,6 @@ import java.util.regex.Pattern;
 public class check {
 
     public static void main(String[] args) {
-        System.out.println(Pattern.matches("[a-zA-Z0-9]+", "a"));
+        System.out.println(Pattern.matches("[a-zA-Z0-9]+", String.valueOf('_')));
     }
 }
