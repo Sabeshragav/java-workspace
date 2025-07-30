328b4ae0ed40d5890e52e0439fcf5129ada4bb16
Sabeshragav <sabeshragav289@gmail.com>
2025-07-30 09:27:58 +0530

    Refactor check class: remove character presence checks and simplify main method with bit swapping example

--- check.java
+++ check.java
@@ -2,48 +2,14 @@
 public class check {
 
     public static void main(String[] args) {
-        String word = "UuE6";
-        for (char ch : word.toLowerCase().toCharArray()) {
-            System.out.println(checkPresence(ch, "sp"));
-        }
+        int a = 6, b = 9;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 
-    static boolean checkPresence(char ch, String checkFor) {
-        if (checkFor.contains("vowel")) {
-            return ch >= 'a' && ch <= 'z' &&
-                    ch == 'a' ||
-                    ch == 'e' ||
-                    ch == 'i' ||
-                    ch == 'o' ||
-                    ch == 'u';
-        }
-        if (checkFor.contains("cons")) {
-            return ch >= 'a' && ch <= 'z' &&
-                    ch != 'a' &&
-                    ch != 'e' &&
-                    ch != 'i' &&
-                    ch != 'o' &&
-                    ch != 'u';
-        }
-        if (checkFor.contains("sp")) {
-            return ch == '@' ||
-                    ch == '#' ||
-                    ch == '$';
-        }
-
-        if (checkFor.contains("digit")) {
-            return ch == '0' ||
-                    ch == '1' ||
-                    ch == '2' ||
-                    ch == '3' ||
-                    ch == '4' ||
-                    ch == '5' ||
-                    ch == '6' ||
-                    ch == '7' ||
-                    ch == '8' ||
-                    ch == '9';
-        }
-
-        return false;
-    }
 }
